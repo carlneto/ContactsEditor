2f49c93f1d09f63ecaeea249d5c00de641bdf2e0
carlneto <carlneto@icloud.com>
2025-10-28 11:13:23 +0000

    Initial Commit

---
+++ ContactsEditor/ContactsEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsEditorApp.swift
+//  ContactsEditor
+//
+//  Created by Carlos Neto on 28/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ContactsEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
