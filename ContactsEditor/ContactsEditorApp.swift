a9191534271ff2952f919cf7d5822df60c98a011
carlneto <carlneto@icloud.com>
2025-11-16 16:07:24 +0000

    adds application Should Terminate After Last Window Closed

--- ContactsEditor/ContactsEditorApp.swift
+++ ContactsEditor/ContactsEditorApp.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 @main
 struct ContactsEditorApp: App {
+   @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
    var body: some Scene {
       WindowGroup {
          ContentView()
             .frame(minWidth: 900, minHeight: 600)
       }
    }
+}
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { true }
 }
